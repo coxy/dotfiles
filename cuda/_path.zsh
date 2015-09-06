4c1915b293b7c68d7e1b138d1db77a706d4fd2cb
coxy <3coxy4@gmail.com>
2015-09-07 00:29:36 +0100

    removing cuda config

--- cuda/_path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH=/Developer/NVIDIA/CUDA-7.0/bin:$PATH
-export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-7.0/lib:$DYLD_LIBRARY_PATH
--- deepdream/_path.zsh
+++
@@ -1,3 +0,0 @@
-export CAFFE_HOME="$HOME/Projects/GIT/caffe"
-export PYTHONPATH="$CAFFE_HOME/distribute/python:$PYTHONPATH"
-export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/lib:$CAFFE_HOME/distribute/lib:$DYLD_FALLBACK_LIBRARY_PATH
